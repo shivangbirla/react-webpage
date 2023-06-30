f92654bd05da0d243e03c551a7ce39ab03b4c95e
shivangbirla <birlashivang01@gmail.com>
2023-07-01 00:17:23 +0530

    react-webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Image from "./components/Image";
+import Reviews from "./components/Review";
+import { first, sec, third, fourth, footerAPI, fifth } from "./data/data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Header first={first} ifExists={true} />
+      <Content sec={sec} />
+      <Header first={fifth} ifExists={false} />
+      <Image third={third} />
+      <Reviews fourth={fourth} />
+      <Footer footerApi={footerAPI} />
     </div>
   );
 }
